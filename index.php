ca8b42d4641bbeabefa9ebe1a9b0ff4035526fbb
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 17:04:18 +0800

    Update and rename MATH.HTML to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head> <P ALIGN= CENTER> <FONT SIZE="13"> MATH FINAL PT </FONT> </CENTER> </head><p>
+<p align= center> <f4> zero o'clock </font>
+ <title> TOMORROW </title>
+<p>
+<p>
+<p>
+<body>
+<center>
+<body bgcolor= "beige">
+<audio src= "" controls </audio>
+</font> 
+</center>
+<p> 
+<p>
+<center>
+<img src= "https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img>
+
+
